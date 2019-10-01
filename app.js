68a7d9d82f725a025c880e79b48c70f3f4ddeab8
Mical Juarez <mical.juarez@gmail.com>
2019-10-01 16:22:10 -0500

    working api query based on user selection. now building out logic to build html elements

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
-const http = new XMLHttpRequest();
+// query the NY Times book API
+function queryAPI(value) {
 
-// NY Times API key
-const key = 'api-key=TXSKC02xZokeRfJYlL4sfCVTDIGe0o8c';
-// Base URL for accessing data
-const baseURL = 'https://api.nytimes.com/svc/books/v3/lists';
+  // http object
+  const http = new XMLHttpRequest();
 
-let url = baseURL + '.json?' + key + '&list=paperback-nonfiction';
+  // NY Times API key
+  const key = 'api-key=TXSKC02xZokeRfJYlL4sfCVTDIGe0o8c';
 
-http.open('GET', url);
-http.send();
+  // base URL for accessing data
+  const baseURL = 'https://api.nytimes.com/svc/books/v3/lists.json?';
 
-http.onreadystatechange = function() {
+  // user input
+  let userGenre = '';
 
-  if (this.readyState == 4 && this.status == 200) {
+  // set userGenre to query format based on user input
+  if (value === 'fiction') {
+    userGenre = 'combined-print-and-e-book-fiction';
+  } else {
+    userGenre = 'combined-print-and-e-book-nonfiction';
+  }
 
-    // Parse response from string to JSON
-    let response = JSON.parse(http.responseText);
+  // query URL based that includes user input
+  let url = baseURL + key + '&list=' + userGenre;
 
-    // log response.results for now to help traverse the obect
-    console.log(response);
+  http.open('GET', url);
+  http.send();
 
-    // Iterate over response and list book titles
-    for (let i = 0; i < response.num_results; i++) {
-      let title = response.results[i].book_details[0].title;
-      let description = response.results[i].book_details[0].description;
-      console.log(title + '\n' + description + '\n' + '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+  http.onreadystatechange = function() {
+
+    if (this.readyState == 4 && this.status == 200) {
+
+      // Parse response from string to JSON
+      let response = JSON.parse(http.responseText);
+
+      // log response.results for now to help traverse the obect
+      console.log(response);
+
+      // Iterate over response and list book titles
+      for (let i = 0; i < response.num_results; i++) {
+        let title = response.results[i].book_details[0].title;
+        let description = response.results[i].book_details[0].description;
+        console.log(title + '\n' + description + '\n' + '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+      }
     }
   }
-}
+
+};
